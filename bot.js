0a422dc8e65457ad98d878b90e0cdc3bbac74c2a
M3roof <43769947+M3roof@users.noreply.github.com>
2019-02-27 10:31:28 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,37 @@ var adminprefix = '!'
 
 
 
-
+const rWlc = JSON.parse(fs.readFileSync("./AutoRole.json", "utf8"));
+client.on('message', message => {
+var prefix = "$";//البرفكس 
+if(message.channel.type === "dm") return;
+if(message.author.bot) return;
+   if(!rWlc[message.guild.id]) rWlc[message.guild.id] = {
+    role: "member"
+  }
+const channel = rWlc[message.guild.id].role
+  if (message.content.startsWith(prefix + "autorole")) {
+    if(!message.member.hasPermission(`MANAGE_GUILD`)) return;
+    let newrole = message.content.split(' ').slice(1).join(" ")
+    if(!newrole) return message.reply(`**$autorole <role name>**`)
+    rWlc[message.guild.id].role = newrole
+    message.channel.send(`**${message.guild.name}'s role has been changed to ${newrole}**`);
+  }
+fs.writeFile("./AutoRole.json", JSON.stringify(rWlc), function(e){
+    if (e) throw e;
+})
+});
+client.on("guildMemberAdd", member => {
+      if(!rWlc[member.guild.id]) rWlc[member.guild.id] = {
+    role: "member"
+  }
+    const sRole = rWlc[member.guild.id].role
+    let Rrole = member.guild.roles.find('name', sRole);
+  member.addRole(Rrole);
+ 
+  
+      
+      });
 
 
 
